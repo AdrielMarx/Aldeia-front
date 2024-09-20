46d34fbbdcd89135a05c947143c5a858c1fe03e6
Adriel Marx <155272647+AdrielMarx@users.noreply.github.com>
2024-09-20 19:02:07 -0300

    :bug: implementação de formulário para cadastro de novos ninjas

--- Aldeia/src/pages/NovoNinja.jsx
+++ Aldeia/src/pages/NovoNinja.jsx
@@ -1,67 +1,132 @@
-import { useForm } from "react-hook-form"
-import { useNavigate } from "react-router-dom"
-import { addNinja } from "../api/ninjas"
-import toast from "react-hot-toast"
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { addNinja } from "../api/ninjas";
+import toast from "react-hot-toast";
+import "../styles/NovoNinja.css";
+import { Button } from "react-bootstrap";
 
-
-function NovoNinja () {
+function NovoNinja() {
   const {
-    register, 
-    handleSubmit, 
+    register,
+    handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function salvarNinja(data) {
-    addNinja(data).then((res) => {
-      toast.success(resposta.message)
-      navigate("/ninjas")
-    }).catch((err) => {
-      toast.error(err.response.data.message)
-    })
+
+    if (data.imgURL === "") data.imgURL = null
+
+    addNinja(data)
+      .then((res) => {
+        console.log("certo")
+        toast.success(res.message);
+        navigate("/ninjas");
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
   }
   return (
     <main>
       <h1>Novo ninja</h1>
       <hr />
-      <div>
-        <label htmlFor="nome">Nome</label>
-        <input 
-        type="text"
-        id="nome"
-        className="form-control"
-        {...register("nome", { required: true, maxLength: 200 })} />
-        {errors.nome && (
-          <small className="text-danger">O nome é inválido!</small>
-        )}
-      </div>
-      <div>
-        <label htmlFor="rank">Rank</label>
-        <select name="rank" id="rank" className="form-select"
-        {...register("rank", { required: true, valueAsNumber: true })}>
-          <option value="" selected disabled>Selelcione o Rank</option>
-          <option value="genin">Genin</option>
-          <option value="chunnin">Chunnin</option>
-          <option value="pJonnin">Primeiro Jonnin</option>
-          <option value="sJonnin">Segundo Jonnin</option>
-          <option value="anbu">ANBU</option>
-          <option value="sannin" >Sannin</option>
-        </select>
-        <select name="aldeia" id="aldeia"
-        className="form-select"
-        {...register("rank", { required: true, valueAsNumber: true })}>
-          <option value="folha">Aldeia da folha</option>
-          <option value="areia">Aldeia da areia</option>
-          <option value="folha">Aldeia da folha</option>
-          <option value="rocha">Aldeia da rocha</option>
-          <option value="nevoa">Aldeia da nevoa</option>
-          <option value="chuva">Aldeia da chuva</option>
-          <option value="outros">Outra</option>
-        </select>
+      <div className="novoNinja">
+        <form onSubmit={handleSubmit(salvarNinja)} className="formNinja">
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            className="form-control"
+            {...register("nome", { required: true, maxLength: 200 })}
+          />
+          {errors.nome && (
+            <small className="text-danger">Nome inválido</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="rank">Rank</label>
+          <select
+            name="rank"
+            id="rank"
+            className="form-select"
+            {...register("rank", { required: true })}
+          >
+            <option value="" selected disabled>
+              Selelcione o Rank
+            </option>
+            <option value="genin">Genin</option>
+            <option value="chunnin">Chunnin</option>
+            <option value="pJonnin">Primeiro Jonnin</option>
+            <option value="sJonnin">Segundo Jonnin</option>
+            <option value="anbu">ANBU</option>
+            <option value="sannin">Sannin</option>
+          </select>
+          {errors.rank && (
+              <small className="text-danger">O rank é obrigatório</small>
+            )}
+        </div>
+        <div>
+          <label htmlFor="aldeia">Aldeia</label>
+          <select
+            name="aldeia"
+            id="aldeia"
+            className="form-select"
+            {...register("aldeia", { required: true })}
+          >
+            <option value="" selected disabled>
+              Selecione a aldeia
+            </option>
+            <option value="folha">Aldeia da folha</option>
+            <option value="areia">Aldeia da areia</option>
+            <option value="rocha">Aldeia da rocha</option>
+            <option value="nevoa">Aldeia da nevoa</option>
+            <option value="chuva">Aldeia da chuva</option>
+            <option value="outros">Outra</option>
+          </select>
+          {errors.aldeia && (
+            <small className="text-danger">A aldeia é obrigatória</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="elemento">Elemento base</label>
+          <select
+            name="elemento"
+            id="elemento"
+            className="form-select"
+            {...register("elemento", { required: true })}
+          >
+            <option value="" selected disabled>
+              Elemento de maior domínio
+            </option>
+            <option value="vento">Vento</option>
+            <option value="fogo">Fogo</option>
+            <option value="terra">Terra</option>
+            <option value="agua">Água</option>
+            <option value="relampago">Relâmpago</option>
+          </select>
+          {errors.elemento && (
+            <small className="text-danger">O elemento é obrigatório</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="imgURL">Foto (opcional)</label>
+          <input type="text"
+          id="imgURL"
+          className="form-control"
+          placeholder="URL da imagem do ninja (max 500 caracteres)."
+          {...register("imgURL", { required: false, maxLength: 500 })} />
+        </div>
+        <div className="botaoNinja">
+          <Button variant="dark" type="submit" className="botao">Cadastrar</Button>
+        </div>
+        
+      </form>
       </div>
     </main>
-  )
+  );
 }
 
-export default NovoNinja
+export default NovoNinja;
